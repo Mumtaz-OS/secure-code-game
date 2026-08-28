9441ff830c145ea62cdb9e92dfed78ed86ab0327
Mumtaz-OS <majid.mumtaz@uwe.ac.uk>
2026-08-28 14:27:35 +0100

    Fix Season 1 Level 1 order validation

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -1,4 +1,4 @@
-'''
+""" '''
 Welcome to Secure Code Game Season-1/Level-1!
 
 Follow the instructions below to get started:
@@ -29,5 +29,50 @@ def validorder(order: Order):
 
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    else:
+        return "Order ID: %s - Full payment received!" % order.id """
+
+'''
+Welcome to Secure Code Game Season-1/Level-1!
+
+Follow the instructions below to get started:
+
+1. tests.py is passing but code.py is vulnerable
+2. Review the code. Can you spot the bug?
+3. Fix the code but ensure that tests.py passes
+4. Run hack.py and if passing then CONGRATS!
+5. If stuck then read the hint
+6. Compare your solution with solution.py
+'''
+
+from collections import namedtuple
+from decimal import Decimal
+
+Order = namedtuple('Order', 'id, items')
+Item = namedtuple('Item', 'type, description, amount, quantity')
+
+MAX_ITEM_AMOUNT = 100000
+MAX_QUANTITY = 100
+MAX_TOTAL = 1e6
+
+def validorder(order: Order):
+    payments = Decimal('0')
+    expenses = Decimal('0')
+
+    for item in order.items:
+        if item.type == 'payment':
+            if -MAX_ITEM_AMOUNT <= item.amount <= MAX_ITEM_AMOUNT:
+                payments += Decimal(str(item.amount))
+        elif item.type == 'product':
+            if type(item.quantity) is int and 0 < item.quantity <= MAX_QUANTITY and 0 < item.amount <= MAX_ITEM_AMOUNT:
+                expenses += Decimal(str(item.amount)) * item.quantity
+        else:
+            return "Invalid item type: %s" % item.type
+
+    if abs(payments) > MAX_TOTAL or expenses > MAX_TOTAL:
+        return "Total amount payable for an order exceeded"
+
+    if payments != expenses:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, payments - expenses)
     else:
         return "Order ID: %s - Full payment received!" % order.id
